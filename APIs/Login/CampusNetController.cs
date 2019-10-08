3ef9568ea78046955bf90617e99650e05b2e9bcf
AM <lgv2800@gmail.com>
2019-10-08 21:30:59 +0200

    Added folder APIs and subfolder Login. Also added basic CampusNetController.

---
+++ APIs/Login/CampusNetController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Freelance_Api.DatabaseAccess;
+using Freelance_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Freelance_Api.APIs.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampusNetController : ControllerBase
+    {
+        
+    }
+}
