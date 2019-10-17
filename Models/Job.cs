852d097c320023564cf9f695b929b45dffc44970
Frederik hjorth <s175109@student.dtu.dk>
2019-10-17 12:00:04 +0200

    modelklassen for job gjort færdig

--- Models/Job.cs
+++ Models/Job.cs
@@ -5,5 +5,11 @@ namespace Freelance_Api.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string id { get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+        public string location { get; set; }
+        public bool monthlyOrHourlyPaid { get; set; }
+        DateTime jobStart { get; set; }
+        DateTime jobEnd { get; set; }
     }
 }
