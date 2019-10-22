2199d579dcbda01ced44c924f27cc9328e62d1cd
Frederik hjorth <s175109@student.dtu.dk>
2019-10-22 16:15:29 +0200

    employeController done

--- Controllers/EmployerController.cs
+++ Controllers/EmployerController.cs
@@ -2,8 +2,70 @@
 
 namespace Freelance_Api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class EmployerController: ControllerBase
     {
+        private readonly EmployerService _employerService;
+
+        public EmployerController(EmployerService employerService)
+        {
+            _employerService = employerService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Employe>> get() =>
+            _employerService.Get();
+
+        [HttpGet("{id:length(24)}")]
+        public ActionResult<Employe> Get(string id)
+        {
+            var employe = _employeService.Get(id);
+
+            if (employe == null)
+            {
+                NotFound();
+            }
+
+            return employe;
+        }
         
+        HttpPost]
+        public ActionResult<Employe> Create(Employe employe)
+        {
+            _employeService.Create(employe);
+
+            return employe;
+        }
+        
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Employe employein)
+        {
+            var emplpye = _employeService.Get(id);
+
+            if (emplpye == null)
+            {
+                return NotFound();
+            }
+
+            _employeService.Update(id, employein);
+
+            return NoContent();
+        }
+        
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var employe = _employeService.Get(id);
+
+            if (employe == null)
+            {
+                return NotFound();
+            }
+
+            _employeService.Remove(employe.Id);
+
+            return NoContent();
+        }
     }
 }
